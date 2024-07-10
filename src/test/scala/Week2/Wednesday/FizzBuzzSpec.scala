6525542fe73c055f4eb2018172a81a7c606d01ec
Joe Higgs <joe.higgs@mercator.group>
2024-07-10 13:00:09 +0100

    Task Setup

---
+++ src/test/scala/Week2/Wednesday/FizzBuzzSpec.scala
@@ -0,0 +1,14 @@
+package Week2.Wednesday
+
+//Create a function called fizzBuzz that takes an Int and returns the same value as a String.
+//
+//• If the input is divisible by 3 (with no remainder) return the word "Fizz"
+//
+//• If the input is divisible by 5 (with no remainder) return the *
+//  word "Buzz".
+//
+//• If the input is divisible by 3 and 5 (with no remainder) return the word "FizzBuzz"
+
+class FizzBuzzSpec {
+
+}
