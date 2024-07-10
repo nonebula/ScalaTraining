8f6966f7d1ddc135316fd3c2533579be06a56ce6
Joe Higgs <joe.higgs@Joes-Laptop.local>
2024-07-10 11:12:04 +0100

    Tests for add and subtract written, passing

--- src/main/scala/Week2/Wednesday/TwoNumbers.scala
+++ src/main/scala/Week2/Wednesday/TwoNumbers.scala
@@ -2,10 +2,13 @@ package Week2.Wednesday
 
 class TwoNumbers (x: Int, y: Int) {
 
-  def add(): Int = ??? //no params as class gives the numbers
+  def add(): Int = {
+    x + y
+  } //no params as class gives the numbers
 
-  def subtract(): Int = ??? //before going to test, navigate to build file and add dependencies
-
+  def subtract(): Int = {
+    x - y
+  } //before going to test, navigate to build file and add dependencies
 }
 
 //AVOID DEPENDENCY TICKETS AT ALL COSTS
--- src/test/scala/Week2/Wednesday/TwoNumbersSpec.scala
+++ src/test/scala/Week2/Wednesday/TwoNumbersSpec.scala
@@ -4,4 +4,15 @@ import org.scalatest.FlatSpec
 
 class TwoNumbersSpec extends FlatSpec {
 
+  "add" should "add numbers" in {
+    val nums = new TwoNumbers(1,2) //new instance of class created. MUST declare new params everytime within test (better practice), unless vals always staying the same, then can be defined at top of document.
+    assert(nums.add == 3)
+  }
+
+  "subtract" should "subtract numbers" in {
+    val nums = new TwoNumbers(3,2)
+    assert(nums.subtract == 1)
+  }
 }
+
+//sbt test will run all tests
