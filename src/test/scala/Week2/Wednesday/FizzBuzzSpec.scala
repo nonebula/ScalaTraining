21f46f59defa4fd89c3bb2ff7f40122e91498ade
Joe Higgs <joe.higgs@mercator.group>
2024-07-17 09:49:27 +0100

    Wk3 file setup

--- src/test/scala/Week2/Wednesday/FizzBuzzSpec.scala
+++ src/test/scala/Week2/Wednesday/FizzBuzzSpec.scala
@@ -4,7 +4,14 @@ import org.scalatest.FlatSpec
 
 class FizzBuzzSpec extends FlatSpec {
 
-  "playGame" should "return Fizz for numbers divisible by 3" in {
+  "playGame" should "return FizzBuzz for numbers divisible by both 3 and 5" in {
+    val n = new FizzBuzz
+    assert(n.playGame(15) == "FizzBuzz")
+    assert(n.playGame(30) == "FizzBuzz")
+    assert(n.playGame(45) == "FizzBuzz")
+  }
+
+  it should "return Fizz for numbers divisible by 3" in {
     val n = new FizzBuzz
     assert(n.playGame(3) == "Fizz")
     assert(n.playGame(6) == "Fizz")
@@ -16,13 +23,6 @@ class FizzBuzzSpec extends FlatSpec {
     assert(n.playGame(5) == "Buzz")
     assert(n.playGame(10) == "Buzz")
     assert(n.playGame(20) == "Buzz")
-  }
-
-  it should "return FizzBuzz for numbers divisible by both 3 and 5" in {
-    val n = new FizzBuzz
-    assert(n.playGame(15) == "FizzBuzz")
-    assert(n.playGame(30) == "FizzBuzz")
-    assert(n.playGame(45) == "FizzBuzz")
   }
 
   it should "return the number as a string for numbers not divisible by 3 or 5" in {
